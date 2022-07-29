c1463ba4efd7447800a440ca6af620922ee92978
Luke Wong <lukewong93@gmail.com>
2022-07-29 17:09:54 +0800

    Fetch data from db

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [pi, setPi] = useState(0);
+
+  useEffect(() => {
+    const fetchPiValue = async () => {
+      const response = await fetch("/api/calculator");
+      const json = await response.json();
+
+      if (response.ok) {
+        const objects = json;
+
+        objects.map((object) => {
+          setPi(object.pi);
+        });
+      }
+    };
+
+    fetchPiValue();
+  }, []);
+
   return (
     <div className="home">
       <h3>Home</h3>
+      <p>Pi = {pi}</p>
     </div>
   );
 };
