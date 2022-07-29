4ec1145e9d7d84f7a5448f9de542d4b8bd5e7698
Luke Wong <lukewong93@gmail.com>
2022-07-29 17:30:27 +0800

    Installed MUI package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
 
 const Home = () => {
   const [pi, setPi] = useState(0);
@@ -23,6 +24,25 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Calculate Circumference of the Sun</h2>
+      <Button
+        variant="contained"
+        style={{
+          marginRight: "1rem",
+          textTransform: "none",
+          borderRadius: "1.25rem",
+        }}
+      >
+        Increase π Accuracy
+      </Button>
+      <Button
+        variant="outlined"
+        style={{
+          textTransform: "none",
+          borderRadius: "1.25rem",
+        }}
+      >
+        Calculate Circumference
+      </Button>
       <p>Formula to calculate circumference = 2 x π x r</p>
       <p>Pi (π) = {pi}</p>
       <p>Radius (r) = 696340 km</p>
